c435115083ab2146559f23dbaa6a2bc8d369f234
iliasZh <76496619+iliasZh@users.noreply.github.com>
2021-06-17 14:08:45 +0500

    Add files via upload

--- RenameScreenshots.ps1
+++ RenameScreenshots.ps1
@@ -1,8 +1,8 @@
-function Get-FormattedDate($filename, [bool]$log)
+function Get-FormattedDate([string]$filename, [bool]$log)
 {
-	$fileInfo = Get-Item $filename										# get file item by name		
-	$fileDate = $fileInfo.LastWriteTime									# get last write time of the file		
-	$dateStr = $fileDate.ToString()										# convert to string to construct the new name		
+	$fileInfo = Get-Item $filename				# get file item by name		
+	$fileDate = $fileInfo.LastWriteTime			# get last write time of the file		
+	$dateStr = $fileDate.ToString()				# convert to string to construct the new name		
 	if ($log) { Write-Host "`n$filename last write time is $dateStr" }
 	$dateAndTime = $dateStr.Substring(6,4) + "." + $dateStr.Substring(3,2) + "." + $dateStr.Substring(0,2) + " - " + $dateStr.Substring(11).Replace(":",".")
 	Return $dateAndTime
@@ -10,7 +10,7 @@ function Get-FormattedDate($filename, [bool]$log)
 
 function Rename-IfNotEqual([string]$oldname, [string]$newname, [bool]$log, [ref]$renamedCounter, [ref]$untouchedCounter)
 {
-	if ($oldname -cne $newname)									# case sensitive not equal
+	if ($oldname -cne $newname)		# case sensitive not equal
 	{
 		Rename-Item -Path $oldname -NewName $newname
 		if ($log) { Write-Host "renamed file $oldname, new name: $newname`n" }
@@ -34,6 +34,13 @@ function Read-YesNoQuestion([string]$prompt, [ref]$flag, [bool]$flip)
 	} while (($ans -ine "y") -and ($ans -ine "n"))
 }
 
+function Write-Borderline
+{
+	Write-Host "------------------------------------------------------------------------------------------------------"
+}
+#------------------------------------------FUNCTIONS-END------------------------------------------#
+#------------------------------------------FUNCTIONS-END------------------------------------------#
+
 Write-Host "File renaming script"
 Write-Host "Renames all specified screenshots to `"GameName yyyy.mm.dd - hh.mm.ss.extension`" (last write date and time)`n"
 
@@ -45,7 +52,7 @@ Write-Host "`n`nFolder search path: $screenshotsFolder"
 $extensions = ".png",".jpg"
 Write-Host "`nFiles with the following extensions can be renamed: $extensions"
 
-Set-Location $screenshotsFolder							# go to My screenshots folder
+Set-Location $screenshotsFolder
 
 $folders = Get-ChildItem -Directory -Name
 Write-Host "`nFound subfolders:"
@@ -56,8 +63,8 @@ ForEach ($folder in $folders)
 	$counter = $counter + 1  
 }
 
-
-$indices = (Read-Host -Prompt "`nEnter the folder numbers of folders you want to choose (enter comma separated values, example - `"1,2,3`")").Split(",").ToInt32($null) | Sort-Object
+$prompt = "`nEnter the folder numbers of folders you want to choose (enter comma separated values, example - `"1,2,3`")"
+$indices = (Read-Host -Prompt $prompt).Split(",").ToInt32($null) | Sort-Object
 
 Write-Host "`nYou chose to rename screenshots in following folders:"
 ForEach ($index in $indices)
@@ -65,24 +72,24 @@ ForEach ($index in $indices)
 	Write-Host ("[$index] " + $folders[$index - 1])
 }
 
-$renameAll = $false
+$renameAll = $false		# permission flag
 $prompt = "`nAsk confirmation for each folder?`n(BE CAREFUL: if you choose NOT to ask, all files will be renamed INSTANTLY!)"
 Read-YesNoQuestion $prompt -flag ([ref]$renameAll) -flip $true 
 
-
 ForEach ($folderIndex in $indices)
 {
+	Write-Host "`n`n"
+	Write-Borderline
+	
 	$folder = $folders[$folderIndex - 1] 
-	
-	$renameAllInFolder = $renameAll
-	
-	Write-Host "`n`n`n------------------------------------------------------------------------------"
+	$renameAllInFolder = $renameAll		# permission flag
 	if (-not ($renameAll))
 	{
 		$continueFlag = $false
 		do
 		{
-			$ans = Read-Host -Prompt "Rename all files in $folder folder? [y] - yes, [a] - ask for each extension, [n] - cancel"
+			$prompt = "Rename all files in $folder folder? [y] - yes, [a] - ask for each extension, [n] - cancel"
+			$ans = Read-Host -Prompt $prompt
 			switch ($ans) 
 			{
 				"y" { $renameAllInFolder = $true }
@@ -95,8 +102,8 @@ ForEach ($folderIndex in $indices)
 		if ($continueFlag)
 		{
 			Write-Host "Renaming in $folder folder canceled!"
-			Write-Host "------------------------------------------------------------------------------"
-			continue
+			Write-Borderline
+			continue	# go to the next iteration immediately
 		}
 	}
 
@@ -104,11 +111,11 @@ ForEach ($folderIndex in $indices)
 
 	ForEach ($extension in $extensions)
 	{
-		$renameCurrExtension = $renameAllInFolder
+		$renameCurrExtension = $renameAllInFolder		# permission flag
 
 		if ($renameAllInFolder -eq $false)
 		{
-			Read-YesNoQuestion "Rename all $extension files?" -flag ([ref]$renameAllInFolder) -flip $false
+			Read-YesNoQuestion "Rename all $extension files?" -flag ([ref]$renameCurrExtension) -flip $false
 		}
 
 		$renamedCounter = 0
@@ -117,25 +124,19 @@ ForEach ($folderIndex in $indices)
 		if (($renameCurrExtension) -or ($renameAllInFolder) -or ($renameAll))
 		{
 			Write-Host "`nSearching for $extension files..."
-	
 			Set-Location $screenshotsFolder	
-			$nameList = Get-ChildItem $folder -Name -Include *$extension			# get a list of names of .png files
+			$nameList = Get-ChildItem $folder -Name -Include *$extension
 
-			if ($nameList.Length -eq 0)
-			{
-				Write-Host "No $extension files found"
-			}
+			if ($nameList.Length -eq 0) { Write-Host "No $extension files found" }
 			else
 			{
 				Write-Host (($nameList.Length).ToString() + " $extension files found")
 				Write-Host "Renaming..."
-				Set-Location ($screenshotsFolder + $folder)								# go inside the folder
-				ForEach ($filename in $nameList)										# for every .png file
+				Set-Location ($screenshotsFolder + $folder)
+				ForEach ($filename in $nameList)
 				{
 					$dateAndTime = Get-FormattedDate $filename $enableLogs
-
 					$newFilename = $folder + " " + $dateAndTime + $extension
-				
 					Rename-IfNotEqual $filename $newFilename $enableLogs ([ref]$renamedCounter) ([ref]$untouchedCounter) 
 				}
 			}
@@ -146,7 +147,7 @@ ForEach ($folderIndex in $indices)
 		}
 	}
 	Write-Host "`nRenaming in $folder folder done!"
-	Write-Host "------------------------------------------------------------------------------"
+	Write-Borderline
 }
 Write-Host "`n`n`nALL DONE!`n"
 Write-Host "Press ENTER to quit"
